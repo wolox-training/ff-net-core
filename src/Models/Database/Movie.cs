85b5e800d4d01c9cd6f45faf9377350ada1e6503
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-05-09 13:54:48 -0300

    List

--- src/Models/Database/Movie.cs
+++ src/Models/Database/Movie.cs
@@ -7,7 +7,6 @@ namespace MvcMovie.Models.Database
     {
         public int Id { get; set; }
         public string Title { get; set; }
-
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }

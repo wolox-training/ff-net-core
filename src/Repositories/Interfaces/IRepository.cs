f938cedcc3a7609c9966b790ebe7d92ba4b93337
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-05-09 15:38:52 -0300

    unity of work fixes

--- src/Repositories/Interfaces/IRepository.cs
+++ src/Repositories/Interfaces/IRepository.cs
@@ -9,12 +9,10 @@ namespace MvcMovie.Repositories.Interfaces
         TEntity Get(int id);
         IEnumerable<TEntity> GetAll();
         IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
-
         TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
 
         void Add(TEntity entity);
         void AddRange(IEnumerable<TEntity> entities);
-        
         void Remove(TEntity entity);
         void RemoveRange(IEnumerable<TEntity> entities);
     }
